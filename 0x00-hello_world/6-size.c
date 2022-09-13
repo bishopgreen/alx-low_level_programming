361f10327c39244f2ea858c248a6fae99e590b10
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-09-13 01:37:23 -0700

    generating assembly code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,24 +4,14 @@
   *
   * sizeof - determines variable size by data type prints to stdio
   *
-  * Return: Always 0 (Success)
+  * Returns zero
   */
 int main(void)
 {
-	char c;
-	int i;
-	float f;
-
-/**
- *  @c,i,f: This is declaring variable types
- * 
- * then getting sizeof using the following method.
- */
-	
-printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of an int: %d byte(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
