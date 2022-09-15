8f8f5e8285a50d1303264f03568cb6f7718158dd
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-09-15 02:49:11 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* main -  prints _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	putchar('putchar');
+	putchar('\n');
+	return (0);
+}
