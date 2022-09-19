304a6798884eb231d0ddff7f8d426b7391b89e07
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-09-19 11:40:03 -0700

    print square shape

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,20 +5,24 @@
 *
 * @size: size of square
 */
+
 void print_square(int size)
 {
-	int i, j;
+	int i;
+	int d;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
+	_putchar(10);
+	else
 	{
-	for (j = 1; j <= size; j++)
-	{
-	_putchar('#');
+		for (i = 0; i < size; i++)
+		{
+			for (d = 1; d < size; d++)
+			{
+			_putchar('#');
+			}
+		_putchar('#');
+		_putchar(10);
+		}
 	}
-	if (i == size)
-	{
-	break;
-	}
-	_putchar('\n');
-	_putchar('\n');
 }
