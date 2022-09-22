6a87ca7678a932f8f4225dfca171b46dd594994f
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-09-22 12:32:25 -0700

    make copies of strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,32 +9,12 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int n;
 	int i;
 
-	n = string_length(src);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i]; i++)
 	{
-	dest[i] = src[i];
+		dest[i] = src[i];
 	}
-	for (; i < n; i++)
-	{
 	dest[i] = '\0';
-	}
 	return (dest);
-}
-/**
-* string_length - finds the length of a string.
-* Return: length of c.
-* @pointer: pointer.
-*/
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-	c++;
-	}
-	return (c);
 }
