05e9b76c0f1bcfdfc2cf6d07a69c70937b510409
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-09-27 20:18:22 -0700

    locate character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,8 +5,8 @@
 * @c: character to be located.
 * Return: pointer to first occurrence of NULL if no character found.
 */
-	char *_strchr(char *s, char c)
-	{
+char *_strchr(char *s, char c)
+{
 	while (*s != '\0')
 	{
 		if (*s == c)
@@ -16,4 +16,4 @@
 		s++;
 	}
 	return (0);
-	}
+}
