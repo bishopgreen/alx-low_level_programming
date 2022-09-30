4e3067cd9090a3e58813d288891c454a0fa4a4ff
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-09-30 09:32:03 -0700

    print the square of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,12 @@ int _sqrt_recursion(int n)
 {
 	int square = 2;
 
-	if (n < 0)
-	return (-1);
-	else if (n == 0 || n == 1)
-	return (n);
-	return (isqrt(n, square));
+	if (n > 0)
+	{
+		return (isqrt(n, square));
+	}
+	else if (n <= 0 || n == 1)
+	{
+		return (n);
+	}
 }
