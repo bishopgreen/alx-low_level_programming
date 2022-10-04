e0b41b067a19d0754c8d0f6cf0dab6c1a6720411
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-10-04 06:40:15 -0700

    print natural squares of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,32 @@
 #include "main.h"
-#include <studio.h>
 /**
-* _sqrt_recursion - function
-* @n: integer to get square root
-* Description: function to find sqroot of given integer
-* Return: 0, return -1 if no square root.
+* _sqrt_recursion - returns square root of a number.
+*
+* @n: number.
+* Return: natural square root of a number.
 */
 int _sqrt_recursion(int n)
 {
-	int c = 0;
+	int square = 2;
 
-	if (c == n)
-	{
-		return (0);
-	}
-	return (0);
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	return (isqrt(n, square));
+}
+/**
+* isqrt - wrapper function to check for squaroot.
+*
+* @n: number to check for squareroot.
+* @i: squareroot.
+* Return: 1 if squareroot is found, -1 if not found else recurse.
+*/
+int isqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (isqrt(n, i + 1));
 }
