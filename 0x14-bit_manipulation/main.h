28b9f0d4761662225e0139750bbc1b2f90738cd2
Kehinde Emmanuel Ayobami <kehindeemmanuela@gmail.com>
2022-11-05 02:56:58 -0700

    add header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -9,4 +9,4 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-#endif
+#endif/*MAIN.H/
